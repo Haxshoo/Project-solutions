2d4721b14c657561ad4d00d9c4c6f55d36471012
Studious Cat <whitecloudlurker@gmail.com>
2023-02-12 17:39:28 +0200

    Create ReverseString.cpp

---
+++ Text/ReverseString.cpp
@@ -0,0 +1,22 @@
+/*
+* Reverse a String
+* Enter a string and the program will reverse it and print it out.
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string userString{};
+
+    std::cout << "Enter a strign to reverse: ";
+    std::getline(std::cin, userString);
+
+    std::reverse(userString.begin(), userString.end());
+    
+    std::cout << userString;
+
+    return 0;
+
+}
